5300adda5ada88b3b0675a2a94886355a101e7f1
MeHuman333 <104174853+MeHuman333@users.noreply.github.com>
2024-09-09 22:30:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,99 +7,23 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
 }
-
-# Define variables
-variable "instance_type" {
-  type    = string
-  default = "t2.micro"
-}
-
-variable "root_pass" {
-  type    = string
-  default = "ansible"
-}
-
-variable "instance_name" {
-  type    = string
-  default = "server"
-}
-
-variable "ssh_private_key" {
-  type    = string
-  default = "/var/lib/jenkins/.ssh/id_ed25519"
-}
-
-variable "ssh_public_key" {
-  type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8sAMf6gNWEHRWfBZIemM2GqUc46MeaH64LZAbTT9ad"
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "allowed_ip" {
-  description = "IP allowed for SSH access"
-  type        = string
-  default     = "3.85.104.164/32"  # Replace with your actual IP address
-}
-
-# Create or import the AWS key pair
 resource "aws_key_pair" "example" {
-  key_name   = "key09"
-  public_key = var.ssh_public_key
-
-  lifecycle {
-    prevent_destroy = true  # Prevent keypair from being destroyed
-    ignore_changes  = [key09]  # Ignore changes to key_name to avoid recreation issues
-  }
+  key_name = "key02"
+  public_key = file("~/.ssh/id_ed25519.pub")
 }
 
-# Security group to allow SSH
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_ip]  # Restrict SSH to the allowed IP
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# EC2 instance configuration
 resource "aws_instance" "server" {
-  ami           = "ami-0522ab6e1ddcc7055"  # Use a valid Ubuntu AMI
+  ami           = "ami-0522ab6e1ddcc7055"
   instance_type = var.instance_type
-  key_name      = aws_key_pair.example.key_name
-  security_groups = [aws_security_group.allow_ssh.name]
+  key_name = "key02"
 
   tags = {
-    Name = var.instance_name
+    Name = "${terraform.workspace}_server"
   }
-
-  # SSH connection configuration
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file(var.ssh_private_key)
-  }
-
-  # Remote-exec provisioner to configure the instance
   provisioner "remote-exec" {
     inline = [
       "cat /etc/os-release",
@@ -108,18 +32,17 @@ resource "aws_instance" "server" {
       "chmod 600 /home/ubuntu/.ssh/authorized_keys",
       "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
     ]
-    retries  = 5        # Retry up to 5 times in case of SSH connection issues
-    timeout  = "5m"     # Set a 5-minute timeout for the remote execution
-    interval = "30s"    # Wait for 30 seconds between retries
   }
-
-  # Local-exec provisioner to generate an Ansible inventory file
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file(var.ssh_private_key)
+   }
   provisioner "local-exec" {
-    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=${var.ssh_private_key}' > inventory.ini"
+    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=~/.ssh/id_ed25519' > inventory.ini"
   }
-
-  # Run the Ansible playbook
   provisioner "local-exec" {
-    command = "ansible-playbook -u ubuntu -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
+        command = "ansible-playbook -u ubuntu -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
   }
 }
