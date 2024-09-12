5d564321f7eeef271b42248175ed0e301a6b4820
MeHuman333 <104174853+MeHuman333@users.noreply.github.com>
2024-09-12 14:49:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,29 +12,33 @@ provider "aws" {
   region     = "ap-south-1"
 }
 resource "aws_key_pair" "example" {
-  key_name = "key-pair"
+  key_name = "key02"
   public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "aws_security_group" "allow_all" {
+  name_prefix = "allow_all"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # This allows all protocols (TCP, UDP, ICMP)
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # This allows all protocols (TCP, UDP, ICMP)
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "server" {
   ami           = "ami-0522ab6e1ddcc7055"
   instance_type = var.instance_type
-  key_name = "key-pair"
-resource "aws_security_group" "allow_ssh" {
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["54.225.23.141/32"]  # Replace with Jenkins or local machine IP
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
+  key_name = "key02"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
     Name = "${terraform.workspace}_server"
@@ -58,6 +62,6 @@ resource "aws_security_group" "allow_ssh" {
     command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=~/.ssh/id_ed25519' > inventory.ini"
   }
   provisioner "local-exec" {
-        command = "ansible-playbook -u ubuntu -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
+        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
   }
 }
